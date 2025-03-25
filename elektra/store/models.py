697f09b596a02c3ae544c6533357f0506c1773ad
Xavier Rolex <xavierrolex7@gmail.com>
2025-03-25 18:51:59 +0600

    Added product Model

--- elektra/store/models.py
+++ elektra/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User #Importing the built-in User model
 
-# Create your models here.
+# product containting all product info
+class Product (models.Model):
+    name = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
