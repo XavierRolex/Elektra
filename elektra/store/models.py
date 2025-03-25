ed7acac4e5200d899081ef50b689ead61e791d2b
Xavier Rolex <xavierrolex7@gmail.com>
2025-03-25 20:34:28 +0600

    added cart model

--- elektra/store/models.py
+++ elektra/store/models.py
@@ -19,4 +19,15 @@ class Wishlist(models.Model):
     def __str__(self):
         return f"{self.user.username} wishlist - {self.product.name}"
 
+# Cart Model
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} (self.quantity)"
+    
+
+
 
