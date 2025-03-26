401d0485c1473aadb13165781cc432803cd3974c
Xavier Rolex <xavierrolex7@gmail.com>
2025-03-27 01:01:07 +0600

    Added cart view

--- elektra/store/views.py
+++ elektra/store/views.py
@@ -21,3 +21,26 @@ def del_from_wishlist(request, product_id):
     product = Product.objects.get(id=product_id)
     Wishlist.objects.filter(user=request.user, product=product).delete()
     return redirect('wishlist')
+
+#Viewing Cart
+@login_required
+def cart(request):
+    cart = Cart.objects.filter(user=request.user)
+    return render(request, 'store/cart.html', {'Cart Items': cart})
+
+#Add to Cart
+@login_required
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    Item, created = Cart.objects.get_or_create(user=request.user, product=product), defaults={'quantity': 1}
+    if not created:
+        Item.quantity += 1
+        Item.save()
+    return redirect('cart')
+
+#Discard from Cart
+@login_required
+def disc_from_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    Cart.objects.filter(user=request.user, product=product).delete()
+    return redirect('cart')
