2d34d48d991df2063f88e426f95de450e4a96d79
Israt24hub <israt.srity@northsouth.edu>
2025-03-29 17:06:54 +0600

    Updated base.html, style.css, and fixed static file configuration

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    featured_products = [
+        {"name": "Product 1", "description": "A great product", "price": 20, "image": "/static/product1.jpg"},
+        {"name": "Product 2", "description": "Another great product", "price": 30, "image": "/static/product2.jpg"},
+    ]
+    return render(request, 'store/home.html', {"featured_products": featured_products})
