7199677481df188e6b71fd6a90a1c74cb4d8f436
Xavier Rolex <xavierrolex7@gmail.com>
2025-04-16 00:45:27 +0600

    unit Testing 1

--- elektra/store/tests.py
+++ elektra/store/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Product, Wishlist
 
-# Create your tests here.
+class WishlistTests(TestCase):
+    def setUp(self):
+        # Set up test user, product, and client
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.product = Product.objects.create(name='Test Product', price=10.0, description='A cool product')
+
+        # Log the user in
+        self.client.force_login(self.user)
+
+    def test_add_to_wishlist(self):
+        response = self.client.get(reverse('add_to_wishlist', args=[self.product.id]))
+        self.assertEqual(response.status_code, 302)  # Expect redirect
+        self.assertTrue(Wishlist.objects.filter(user=self.user, product=self.product).exists())
+
+    def test_remove_from_wishlist(self):
+        Wishlist.objects.create(user=self.user, product=self.product)
+        response = self.client.get(reverse('remove_from_wishlist', args=[self.product.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Wishlist.objects.filter(user=self.user, product=self.product).exists())
+
+    def test_view_wishlist(self):
+        Wishlist.objects.create(user=self.user, product=self.product)
+        response = self.client.get(reverse('view_wishlist'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.product.name)
